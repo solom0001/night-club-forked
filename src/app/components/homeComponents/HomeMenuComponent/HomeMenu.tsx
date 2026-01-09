dd8a6a078b65947fe50d1902046cc918dbd0e893
Oliver <oliverbengtsson67@gmail.com>
2026-01-09 11:24:13 +0100

    +++

---
+++ src/app/components/homeComponents/HomeMenuComponent/HomeMenu.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+const HomeMenu = () => {
+  const [hidden, setHidden] = useState(false);
+  const [isRendered, setIsRendered] = useState(true);
+
+  const menu = ["events", "gallery", "music", "video", "profils", "blog"];
+
+  const toggleMenu = () => {
+    if (hidden) {
+      setIsRendered(true);
+      setHidden(false);
+    } else {
+      setHidden(true);
+    }
+  };
+
+  return (
+    <div className="w-[80px] hidden 2xl:sticky 2xl:top-60 mt-[15rem] ml-auto mr-8 2xl:block z-100 col-3! row-span-3">
+      <h1
+        onClick={toggleMenu}
+        className={`${
+          hidden ? "" : "rotate-45"
+        } text-center text-[45px]! text-(--red) bold text-shadow-[0_0_7px_black] hover:text-white transition-all duration-200 ease-in`}
+      >
+        +
+      </h1>
+
+      <ul
+        onAnimationEnd={() => {
+          if (hidden) {
+            setIsRendered(false);
+          }
+        }}
+        className={`
+          ${hidden ? "slide-out" : "slide-in"}
+          ${!isRendered ? "hidden" : ""}
+          flex flex-col text-center [&>*]:text-(--red)
+        `}
+      >
+        <li>
+          <Link href="#welcome">
+            <p className="bold text-shadow-[0_0_7px_black] hover:text-white hover:scale-[1.1] transition-all duration-200 ease-in">
+              WELCOME
+            </p>
+          </Link>
+        </li>
+
+        {menu.map((n) => (
+          <li key={n}>
+            <span className="block shadow-[0_0_7px_black] bg-(--red) h-8 w-[2px] mx-auto my-4"></span>
+            <Link href={`#${n}`}>
+              <p className="bold text-shadow-[0_0_7px_black] uppercase hover:text-white hover:scale-[1.1] transition-all duration-200 ease-in">
+                {n}
+              </p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default HomeMenu;
